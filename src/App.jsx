436d7b193543b391c5b66416ce58072e40914f9b
Thiago Zell Mendes <thiagosmendes.th@gmail.com>
2023-04-24 22:37:27 -0300

    Feat: CreateSidebarComponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import { Header } from "./components/Header";
+import { Post } from "./Post";
+import { Sidebar } from "./components/SideBar";
+
+import styles from "./App.module.css";
+
+import "./global.css";
+
 
 function App() {
   return (
     <div>
       <Header />
-      <h1>Hello World</h1>
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Thiago Mendes" content="Melhor de todos!" />
+          <Post author="Karana Chughi" content="Melhor de todas!" />
+        </main>
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,25 @@
+import styles from "./Sidebar.module.css";
+import { PencilLine } from "phosphor-react";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1607705703571-c5a8695f18f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGNvZGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+      />
+
+      <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/ThiagoZellMendes.png" />
+        <strong>Thiago Mendes</strong>
+        <span>Mobile Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20}/>
+          Editar seu perfil</a>
+      </footer>
+    </aside>
+  );
+}
