46eb558a7eff52e27f92eb2dc8f64c7f81fa88e6
Thiago Zell Mendes <thiagosmendes.th@gmail.com>
2023-04-29 20:32:13 -0300

    Refactor: ApplyDataMocksOnPosts

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Header } from "./components/Header";
 import { Post } from "./components/Post";
 import { Sidebar } from "./components/SideBar";
+import { posts } from "./dataPost";
 
 import styles from "./App.module.css";
 
 import "./global.css";
-
 
 function App() {
   return (
@@ -15,8 +15,17 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post author="Thiago Mendes" content="Melhor de todos!" />
-          <Post author="Karana Chughi" content="Melhor de todas!" />
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
+          ˝
         </main>
       </div>
     </div>
